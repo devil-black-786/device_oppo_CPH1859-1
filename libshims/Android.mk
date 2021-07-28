b8e1c754bbb318d044262c82b8854f4a6ac54ed0
Rashed Abdel-Tawab <rashed@linux.com>
2021-07-28 07:57:36 +0000

    CPH1859: libshims: libnvram: Move to vendor
    * Needed for Full VNDK compatibility
    
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -4,5 +4,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE      := libshim_nvram
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES   := libnvram/libshim_nvram.cpp
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
-
