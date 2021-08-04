6b51d1c6b180cdd719f04e5a214e364d8d4c90c2
devil-black-786 <fahimahmad904@gmail.com>
2021-08-04 09:38:30 +0530

    CPH1859: Switch to redfin fp

--- lineage_CPH1859.mk
+++ lineage_CPH1859.mk
@@ -49,7 +49,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="CPH1859" \
     PRIVATE_BUILD_DESC="full_oppo6771_17065-user 9 PPR1.180610.011 eng.root.20200509.060522 release-keys"
 
-BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RQ2A.210405.005/7181113:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
 
 # Time
 LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
